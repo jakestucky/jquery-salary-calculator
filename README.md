a55077e3e25a7d7a76c85960338e2017c871e4d0
Jake Stucky <jakestucky@gmnail.com>
2020-08-29 11:55:52 -0500

    added delete button

--- README.md
+++ README.md
@@ -13,8 +13,8 @@ Additional README details can be found [here](https://github.com/PrimeAcademy/re
 [x] Create input form with following fields :employee first name, last name, ID number, job title, annual salary
 [x] Create table for data to live in
 [x] Create submit button
-[ ] Onlick listener to store data in an array as objects
-[ ] Have the onclick append the item added to li
+[x] Onlick listener to store data in an array as objects
+[x] Have the onclick append the item added to table
 [ ] Create function to use stored salary data and ensure total month cost < 20,000 or add a red background to that <li>
 [ ] Create delete button
 [ ] Onclick listener to delete an employee 9inl;ine with the <li>
--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 $(document).ready(onReady);
 
 let employeeData = [];
+const maxSalary = 20000;
+let currentSalary = 0;
 
 function onReady() {
   $('#addEmployeeBtn').on('click', addEmployee);
+  displayEmployees();
 }
 function addEmployee() {
+  //function tostore data in an array as objects
   console.log('in addEmployee');
   const newEmployee = {
     firstName: $('#empFirstNameIn').val(),
@@ -14,6 +18,31 @@ function addEmployee() {
     empTitle: $('#empTitleIn').val(),
     empSalary: $('#empSalaryIn').val(),
   };
+  currentSalary += $('#empSalaryIn').val();
   employeeData.push(newEmployee);
-  console.log(employeeData);
-}
+  displayEmployees();
+  //resaet the form fields to empty after click function
+  $('#empFirstNameIn').val('');
+  $('#empLastNameIn').val('');
+  $('#empNumberIn').val('');
+  $('#empTitleIn').val('');
+  $('#empSalaryIn').val();
+} // end addEmployee
+function displayEmployees() {
+  console.log('in displayEmployees');
+  // target an ul element on DOM
+  let el = $('#empData');
+  // empty el
+  el.empty();
+  // loop through inventory
+  for (let i = 0; i < employeeData.length; i++) {
+    // append each item to the  table body]
+    el.append(`<tr>
+     <td>${employeeData[i].firstName}</td>
+      <td>${employeeData[i].lastName}</td>
+        <td> ${employeeData[i].empID}</td>
+        <td> ${employeeData[i].empTitle}</td>
+        <td> ${employeeData[i].empSalary}</td>
+        <td> <button class="deleteBtn">Delete</button> </td> </tr>`);
+  } // end for
+} // end displayEmployees
